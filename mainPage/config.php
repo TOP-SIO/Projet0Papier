fb12f091c706ca5bfb801182f9c8fc5e224e88a0
ThomasSengatKuo <tsengatkuo@aforp.eu>
2023-06-11 16:43:15 +0200

    config server fix

--- mainPage/config.php
+++ mainPage/config.php
@@ -1,10 +1,6 @@
 <?php
     define('ROOT_PATH', realpath(dirname(__FILE__)));
-<<<<<<< HEAD
-    define('BASE_URL', 'http://localhost:8888/Projet0Papier/mainPage/');
-=======
     define('BASE_URL', 'http://192.168.120.51');
->>>>>>> 774f9d254384988088e63a08df18287b979a05a1
 
     session_start();
     $host = 'localhost';
